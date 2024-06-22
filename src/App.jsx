997985d1e2fd6469d7518b25e00b47ed421c649c
Luiz Carlos Da Silva Costa <luiz0519@DIGITALCOLLEGE.LOCAL>
2024-06-22 11:51:00 -0300

    app modifiado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 
+
+function Header({children}) {
+  return (
+    
+    <header className="bg-white-500">
+      <nav className="mx-auto flex items-center p-6 shadow-md">
+        {children}       
+      </nav>
+    </header>
+  )
+}
+
+function HeaderLink({href,children}) {
+  return (
+
+    <a href={href} classeName="font-semibold leading-6 text-black" >{children}</a>
+  )
+  
+}
+
+
+
+
 function App() {
 
   return (
-    
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>        
+      <Header>
+        <HeaderLink href="#">Company</HeaderLink>
+        <HeaderLink href="#">MarketPlace</HeaderLink>
+      </Header> 
+           
+    </>   
   )
 }
 
